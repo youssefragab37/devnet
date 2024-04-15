e6e454ecaa7886c365196a33d653f4f703d5d697
youssefragab37 <youssefrajab7@gmail.com>
2024-04-15 11:30:09 +0200

    hello

---
+++ test.py
@@ -0,0 +1,9 @@
+from netmiko import ConnectHandler
+vxr=ConnectHandler(host="192.168.126.206",username="youssef",password="Welcome@2024",device_type="cisco_ios")
+print(vxr.find_prompt())
+vxr.enable()
+print(vxr.find_prompt())
+vxr.config_mode()
+print(vxr.find_prompt())
+show=vxr.send_command_timing("do sh ip in br")
+print(show)
